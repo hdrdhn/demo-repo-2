197ef8cddc76b4f11b135cbb84f70c8dfef6ecc7
Hadar Dahan <hadar.dahan@mail.huji.ac.il>
2021-03-11 15:43:15 +0200

    added hello world
    some description

---
+++ hello_world.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hdrdh on 11/03/2021.
+//
+
+#include "hello_world.h"
---
+++ hello_world.h
@@ -0,0 +1,13 @@
+//
+// Created by hdrdh on 11/03/2021.
+//
+
+#ifndef GIT_HELLO_WORLD_H
+#define GIT_HELLO_WORLD_H
+
+
+class hello_world {
+};
+
+
+#endif//GIT_HELLO_WORLD_H
